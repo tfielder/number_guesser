3416a4179a220f00a6fa99038cf688876baa5eb4
Tim Fielder <fiel1139@gmail.com>
2018-11-19 12:52:46 -0700

    stopping point.

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".guess_button").on ('click', (function() {
+    alert("You clicked it!");
+  });
+});
+
+//create a var
+//user types in a number
+//checks to see if the number is between 1 to 100
+//stores the number into the var
+//determines if the number is higher or lower
