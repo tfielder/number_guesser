b9a397e8284b06a726a469f00f730e9ef16af768
Tim Fielder <fiel1139@gmail.com>
2018-11-21 13:29:05 -0700

    updates css, divides up classes to function properly with jquery.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
   console.log("JavaScript is connected");
   var aRandomNumber = Math.floor((Math.random() * 100) + 1);
-  //var userGuess = null;
-  $(".js-guess-button, button").click(function() {
-    console.log("The random number" + aRandomNumber);
+  console.log("random number is " + aRandomNumber);
+
+  //Guess Button
+  $(".js-guess-button").click(function() {
     var userGuess = $('#user-guess').val();
+    
     if (userGuess < aRandomNumber) {
       $('.result-line').text("That is too low");
     } else if (userGuess > aRandomNumber) {
@@ -12,8 +14,14 @@ $(document).ready(function() {
     } else {
       $('.result-line').text("BOOM!");
     }
-    //$('#user-guess').text("");
-    $('.first-line').text("Your last guess was")
+
+    $('.first-line').text("Your last guess was");
     $('.guess-text').text(userGuess);
   });
+
+  //Clear Button
+  $(".js-clear-button").click(function () {
+    $('#user-guess').val("");
+  });
+
 });
